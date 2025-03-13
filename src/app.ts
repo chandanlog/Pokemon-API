bb77a919c063633431ef85b859f9e609fe081f5a
Abhishek <jha46342@gmail.com>
2025-03-13 16:51:41 +0400

    gitignore and port change

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1/pokemon", pokemon);
 app.use(RouteNotFoundMiddleware);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on: ${PORT}`);
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+  console.log(`Server is listening on: ${port}`);
 });
